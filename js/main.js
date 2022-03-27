d647e00e6410718a6c528e4292b0b0349a217d73
Daniel-Esteban-Lahti-NTI-Johanneberg <daniel.estebanlahti@elev.ga.ntig.se>
2022-03-27 22:55:56 +0200

    Mobile Resp sida 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-document.querySelector("#fadeLayer").addEventListener("click",showDropMenu);
-
 document.querySelector(".dropDownMenu").addEventListener("click",showDropMenu);
 
 document.querySelector("#dropDownIcon").addEventListener("click",iconAnimation);
@@ -10,9 +8,6 @@ document.querySelector("#dropDownIcon").addEventListener("click",showDropMenu);
 function showDropMenu(){
     let dropMenu = document.querySelector(".dropDownMenu");
     dropMenu.classList.toggle("show");
-
-    let fadeLayer = document.querySelector("fadeLayer");
-    fadeLayer.classList.toggle("visible");
 }
 
 function iconAnimation(){
